68077ac829620f50027005a236c438b56b325b57
matheusvilar2019 <matheusvilar2016@gmail.com>
2020-07-31 21:46:20 -0300

    Rodapé (novo), Resumo do Autor (Melhorado)

--- src/app/livro-por-autor/livro-por-autor.component.ts
+++ src/app/livro-por-autor/livro-por-autor.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class LivroPorAutorComponent implements OnInit {
   http: HttpClient;
   livros: Object[] = [];
+  autor: Object[] = [];
   nomeAutor: string;
 
   constructor(http: HttpClient, private route: ActivatedRoute) {
@@ -21,13 +22,25 @@ export class LivroPorAutorComponent implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.params.nomeAutor);
     this.nomeAutor = this.route.snapshot.params.nomeAutor;
+    this.getAutor();
     this.getLivros();
+    
   }
 
   getLivros(){
     this.http
       .get<Object[]>('http://localhost:9090/biblioteca/livro/autor/' + this.nomeAutor)
       .subscribe(livros => this.livros = livros);
+  }
+
+  getAutor(){
+    this.http
+      .get<Object[]>('http://localhost:9090/biblioteca/autor/nome/' + this.nomeAutor)
+      .subscribe(autor => this.autor = autor);
+
+      this.http
+      .get<Object[]>('http://localhost:9090/biblioteca/autor/nome/' + this.nomeAutor)
+      .subscribe(autor => console.log(autor));
   }
 
 }
